e6f489b558b9d0c89ce63ba90c490812a9d2e5d7
stark-code-css <113598257+stark-code-css@users.noreply.github.com>
2025-05-02 21:24:57 +0530

    Update Program.cs

--- CourtRoom/Program.cs
+++ CourtRoom/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: "MyCorsPolicy", policy =>
     {
-        policy.WithOrigins("http://localhost:5173/");
+        policy.WithOrigins("https://courtroom.onrender.com", "https://localhost:7143", "http://localhost:5205");
         policy.AllowAnyHeader();
         policy.AllowAnyMethod();
         policy.AllowCredentials();
